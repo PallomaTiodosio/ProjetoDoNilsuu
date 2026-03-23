32e685e7cd9eaec87e1939cbb0fc13f1964ba5e2
PallomaTiodosio <tiodosiop@gmail.com>
2026-03-22 21:12:44 -0300

    Crindo Projeto

---
+++ AplicacaoBanco/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace AplicacaoBanco.Models
+{
+    public class Usuario
+    {
+        [Display(Name="Código")]
+        public int IdUsu { get; set; }
+
+        [Display(Name = "Nome Completo")]
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        public string? nomeUsu { get; set; }
+
+        [Display(Name = "Cargo")]
+        [Required(ErrorMessage = "O campo Cargo é obrigatório")]
+        public string? Cargo { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        [Display(Name = "Nascimento")]
+        [Required(ErrorMessage = "O campo nascimento é obrigatório")]
+        //[DataType(DataType.DateTime)]
+        public DateTime DataNasc { get; set; }
+    }
+    
+}
